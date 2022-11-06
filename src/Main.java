fc45ddaac053f90c83a120d936f917e0f5faaba8
AndreyVech <icq8039607@yandex.ru>
2022-11-06 11:34:33 +0300

    for interview

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        String str1;
+        String str2;
+        String str3;
+        int a;
+        int b;
+        int c;
+        int d;
+    }
+}
